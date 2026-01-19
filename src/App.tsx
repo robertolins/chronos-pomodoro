e58ae2ce21f3d027ab6e16e47e1dce305bcfb694
Roberto <roberto.junior@ewally.com.br>
2026-01-19 16:17:56 -0300

    feat: cria um form e estiliza o input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Container from './components/Container';
 import { CountDown } from './components/CountDown';
+import DefaultInput from './components/DefaultInput';
 import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
@@ -19,6 +20,32 @@ export default function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              id='meu-input'
+              labelText='task'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
 
       <Container>
