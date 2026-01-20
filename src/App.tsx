67c7fea980682720a79610f23e4458b4a2367ee0
Roberto <roberto.junior@ewally.com.br>
2026-01-20 10:35:42 -0300

    feat: cria template para as páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,7 @@
-import Container from './components/Container';
-import { CountDown } from './components/CountDown';
-import DefaultInput from './components/DefaultInput';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { Cycles } from './components/Cycles';
 import './styles/global.css';
 import './styles/theme.css';
-import DefaultButton from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 export default function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              id='meu-input'
-              labelText='task'
-              type='text'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon size={32} />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
