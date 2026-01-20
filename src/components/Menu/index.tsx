6249e39f37ca83fd3b8c497d59eca2c6977fc1d2
Roberto <roberto.junior@ewally.com.br>
2026-01-20 09:55:56 -0300

    feat: permite alterar tema da aplicação

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,72 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(
+    () => (localStorage.getItem('theme') as AvailableThemes) ?? 'dark',
+  );
+
+  const themeIcon = {
+    dark: <SunIcon size={24} />,
+    light: <MoonIcon size={24} />,
+  };
+
+  const handleThemeChange = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+
+    setTheme(prevTheme => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
         <HouseIcon size={24} />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver histórico'
+        title='Ver histórico'
+      >
         <HistoryIcon size={24} />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon size={24} />
       </a>
-      <a className={styles.menuLink} href='#'>
-        <SunIcon size={24} />
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Alterar o tema'
+        title='Alterar o tema'
+        onClick={handleThemeChange}
+      >
+        {themeIcon[theme]}
       </a>
     </nav>
   );
