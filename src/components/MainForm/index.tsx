6a7b90f391d3a55112918009ee0554e26742540f
Roberto <roberto.junior@ewally.com.br>
2026-01-27 23:14:24 -0300

    feat: personaliza mensagens da tarefa

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,6 +8,7 @@ import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
+import { Tips } from '../Tips';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -63,7 +64,7 @@ export function MainForm() {
       </div>
 
       <div className='formRow'>
-        <p>Lorem ipsum dolor sit amet.</p>
+        <Tips />
       </div>
 
       {state.currentCycle > 0 && (
