cae609a9b4160c929926faeb669748d5b4bc6f83
Roberto <roberto.junior@ewally.com.br>
2026-01-28 00:49:26 -0300

    feat: criar um web worker para fazer a atualização do contador

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,8 @@ import { useEffect, useReducer } from 'react';
 import { TaskContext } from './TaskContext';
 import { initialTaskState } from './initialTaskState';
 import { taskReducer } from './taskReducer';
+import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
+import { TaskActionTypes } from './taskActions';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -10,9 +12,34 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onMessage(({ data }) => {
+    const countDownSeconds = data;
+
+    if (countDownSeconds <= 0) {
+      dispatch({
+        type: TaskActionTypes.COMPLETE_TASK,
+      });
+      worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
+      });
+    }
+  });
+
   useEffect(() => {
-    console.log({ state });
-  }, [state]);
+    console.log(state);
+
+    if (!state.activeTask) {
+      console.log('Worker terminado por falta de activeTask');
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [state, worker]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
